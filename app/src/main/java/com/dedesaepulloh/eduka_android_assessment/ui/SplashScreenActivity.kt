68d1c8205dc261967273f1dff2712e30d81f0cf7
dedesaepulloh <dedesaepulloh77@gmail.com>
2021-09-14 11:44:15 +0700

    add splash screen

---
+++ app/src/main/java/com/dedesaepulloh/eduka_android_assessment/ui/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package com.dedesaepulloh.eduka_android_assessment.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dedesaepulloh.eduka_android_assessment.MainActivity
+import com.dedesaepulloh.eduka_android_assessment.R
+import com.dedesaepulloh.eduka_android_assessment.databinding.ActivitySplashScreenBinding
+import com.dedesaepulloh.eduka_android_assessment.utils.Helper.TIME_MILLIS
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var screenBinding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        screenBinding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(screenBinding.root)
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(TIME_MILLIS)
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/dedesaepulloh/eduka_android_assessment/utils/Helper.kt
@@ -0,0 +1,5 @@
+package com.dedesaepulloh.eduka_android_assessment.utils
+
+object Helper {
+    const val TIME_MILLIS = 3000L
+}
